c3bbddeeded10de7b25a71544d19ea27a2f69f72
JoelMoralesa <Jmorales@live.se>
2020-12-01 17:55:46 +0100

    last commit

--- js/main.js
+++ js/main.js
@@ -16,13 +16,6 @@ http://www.omdbapi.com/?apikey=669ca14c&i=
 
 
 
-NÄST SISTA (första API parameter)
-http://www.omdbapi.com/?apikey=669ca14c&s=Rogue%20One:%20A%20Star%20Wars&Y=2017
-
-SISTA FILMEN (andra API parametern)
-http://www.omdbapi.com/?apikey=669ca14c&s=Star%20Wars:%20Episode%20IX&type=movie
-
-
 */
 
 /**
@@ -55,9 +48,47 @@ http://www.omdbapi.com/?apikey=669ca14c&s=Star%20Wars:%20Episode%20IX&type=movie
 function showMovie(sw) {
     var url = "http://www.omdbapi.com/?apikey=669ca14c&i=" + sw,
 
-    //hämta ny data när man ändrar film bland alternativen
+    //skapa en ny variabel som hämtar ny data när man ändrar film bland alternativen
     dataRequest = new XMLHttpRequest(),
     
 
+    //skapa variabel som länkas till "img" i HTML så filmens poster kan läggas där
+    poster = document.querySelector("img");
 
+    //öppna en request till URL:en för att hämta data
+    dataRequest.open('GET', url);
+
+    //ange att jag vill ha tillbaka datan i .responseType = 'json'; för att få objects tillbaka, för att enkelt kunna komma åt den olika datan man får tillbaka
+    dataRequest.responseType = "json";
+
+    //skicka requesten till servern
+    dataRequest.send();
+
+
+
+
+    //skapa en till function som applicerar datan hämtat från URL. Endast efter att allting har hämtats från API:n
+
+    dataRequest.onload = function() {
+        //skapar en variabel "data" som sparar all respons från requesten
+        var data = dataRequest.response;
+
+        //snappa upp filmens poster källa och applicera det i src i html
+        poster.src = `${data.Poster}`;
+
+        //lägg till alt. till bilden,  dvs kort info om bilden. snappa upp filmens text + titel 
+        poster.alt = `Movie poster for ${data.Title}`;
+
+        //snappa upp filmens titel samt år den kom ut. applicera det i den tomma content div:en
+        content.innerHTML = ` <h2>${data.Title} ${data.Year} </h2>`;
+
+        //snappa upp filmens beskrivning 
+        content.innerHTML += `<p id="p1">${data.Plot} </p><br> <br> <p id="p2">I rate this movie 1/5, based on the only fact that I have never seen any Star Wars movie.</p> <p id="p3">Ever. </p>`;
+    }
 }
+
+//använd movies från listan i html. "change" attributen ändrar det som visas beroende på vilken film man tryckt på.
+movies.addEventListener("change", function() {
+    showMovie(movies.value);
+})
+showMovie(movies.value);
